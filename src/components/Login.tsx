39443ed256a55fb21ab2ff712132c2cf8bd1ee83
Roman Martyniak <roman.martunyak@gmail.com>
2024-10-11 16:01:13 +0300

    add input state & handlers

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,17 +1,47 @@
+import { useState } from "react";
+
 export default function Login() {
+  const [value, setValue] = useState({
+    email: "",
+    password: "",
+  });
+
+  function handleSubmit(event: React.FormEvent) {
+    event.preventDefault();
+  }
+
+  function handleChange(identifier, value: string) {
+    setValue((prevValue) => ({
+      ...prevValue,
+      [identifier]: value,
+    }));
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={(event) => handleChange("email", event.target.value)}
+            value={value.email}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={(event) => handleChange("password", event.target.value)}
+            value={value.password}
+          />
         </div>
       </div>
 
